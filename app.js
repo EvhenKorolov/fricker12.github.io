23a1f238e1d03f2186294e8fbec57a9953a35700
Evhen Korolov <fricker12@gmail.com>
2024-09-14 16:22:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = '#2cab37';
 
-// Устанавливаем текст кнопки и показываем ее сразу, чтобы убедиться, что она работает
-tg.MainButton.setText("Отправить");
-tg.MainButton.show();
-
 // Получаем элементы HTML
 let sendLinkBtn = document.getElementById("sendLinkBtn");
 let linkInput = document.getElementById("linkInput");
