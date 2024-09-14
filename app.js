df0c70ea8bb8f0dca6d7036595c9d03a521f32f5
Evhen Korolov <fricker12@gmail.com>
2024-09-14 22:51:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,26 +9,25 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+// Массив с кнопками
+let buttons = document.querySelectorAll(".btn");  // Предположим, что у всех кнопок есть класс 'btn'
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали жертву №1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+// Цикл для назначения обработчиков
+buttons.forEach((button, index) => {
+    button.addEventListener("click", function(){
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            let buttonNumber = index + 1;
+            tg.MainButton.setText(`Вы выбрали жертву №${buttonNumber}!`);
+            item = buttonNumber.toString();
+            tg.MainButton.show();
+        }
+    });
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
+    tg.sendData(item);
 });
 
 
